4aeed3c1ac9f36572c8ba38c84e3225f877d508e
hashtag8 <25273994+hashtag8@users.noreply.github.com>
2026-03-01 09:01:34 -0800

    Add files via upload

---
+++ libHKWirelessHD.a.h
@@ -0,0 +1,163 @@
+typedef unsigned char   undefined;
+
+typedef unsigned int    dword;
+#define unkbyte9   unsigned long long
+#define unkbyte10   unsigned long long
+#define unkbyte11   unsigned long long
+#define unkbyte12   unsigned long long
+#define unkbyte13   unsigned long long
+#define unkbyte14   unsigned long long
+#define unkbyte15   unsigned long long
+#define unkbyte16   unsigned long long
+
+#define unkuint9   unsigned long long
+#define unkuint10   unsigned long long
+#define unkuint11   unsigned long long
+#define unkuint12   unsigned long long
+#define unkuint13   unsigned long long
+#define unkuint14   unsigned long long
+#define unkuint15   unsigned long long
+#define unkuint16   unsigned long long
+
+#define unkint9   long long
+#define unkint10   long long
+#define unkint11   long long
+#define unkint12   long long
+#define unkint13   long long
+#define unkint14   long long
+#define unkint15   long long
+#define unkint16   long long
+
+#define unkfloat1   float
+#define unkfloat2   float
+#define unkfloat3   float
+#define unkfloat5   double
+#define unkfloat6   double
+#define unkfloat7   double
+#define unkfloat9   long double
+#define unkfloat11   long double
+#define unkfloat12   long double
+#define unkfloat13   long double
+#define unkfloat14   long double
+#define unkfloat15   long double
+#define unkfloat16   long double
+
+#define BADSPACEBASE   void
+#define code   void
+
+typedef struct linker_option_command linker_option_command, *Plinker_option_command;
+
+struct linker_option_command {
+    dword cmd;
+    dword cmdsize;
+    dword count;
+};
+
+typedef struct section section, *Psection;
+
+struct section {
+    char sectname[16];
+    char segname[16];
+    dword addr;
+    dword size;
+    dword offset;
+    dword align;
+    dword reloff;
+    dword nrelocs;
+    dword flags;
+    dword reserved1;
+    dword reserved2;
+};
+
+typedef struct dysymtab_command dysymtab_command, *Pdysymtab_command;
+
+struct dysymtab_command {
+    dword cmd;
+    dword cmdsize;
+    dword ilocalsym;
+    dword nlocalsym;
+    dword iextdefsym;
+    dword nextdefsym;
+    dword iundefsym;
+    dword nundefsym;
+    dword tocoff;
+    dword ntoc;
+    dword modtaboff;
+    dword nmodtab;
+    dword extrefsymoff;
+    dword nextrefsyms;
+    dword indirectsymoff;
+    dword nindirectsyms;
+    dword extreloff;
+    dword nextrel;
+    dword locreloff;
+    dword nlocrel;
+};
+
+typedef struct segment_command segment_command, *Psegment_command;
+
+struct segment_command {
+    dword cmd;
+    dword cmdsize;
+    char segname[16];
+    dword vmaddr;
+    dword vmsize;
+    dword fileoff;
+    dword filesize;
+    dword maxprot;
+    dword initprot;
+    dword nsects;
+    dword flags;
+};
+
+typedef struct symtab_command symtab_command, *Psymtab_command;
+
+struct symtab_command {
+    dword cmd;
+    dword cmdsize;
+    dword symoff;
+    dword nsyms;
+    dword stroff;
+    dword strsize;
+};
+
+typedef struct mach_header mach_header, *Pmach_header;
+
+struct mach_header {
+    dword magic;
+    dword cputype;
+    dword cpusubtype;
+    dword filetype;
+    dword ncmds;
+    dword sizeofcmds;
+    dword flags;
+};
+
+typedef struct linkedit_data_command linkedit_data_command, *Plinkedit_data_command;
+
+struct linkedit_data_command {
+    dword cmd;
+    dword cmdsize;
+    dword dataoff;
+    dword datasize;
+};
+
+
+
+
+undefined +[HKWDeviceEventHandlerSingleton_sharedInstance]();
+undefined ___48+[HKWDeviceEventHandlerSingleton_sharedInstance]_block_invoke();
+undefined -[HKWDeviceEventHandlerSingleton_handleDeviceStateUpdated:withReason:]();
+undefined ___70-[HKWDeviceEventHandlerSingleton_handleDeviceStateUpdated:withReason:]_block_invoke();
+undefined ___copy_helper_block_();
+undefined ___destroy_helper_block_();
+undefined -[HKWDeviceEventHandlerSingleton_handleErrorOccurred:withErrorMessage:]();
+undefined ___71-[HKWDeviceEventHandlerSingleton_handleErrorOccurred:withErrorMessage:]_block_invoke();
+undefined ___copy_helper_block_12();
+undefined ___destroy_helper_block_13();
+undefined -[HKWDeviceEventHandlerSingleton_delegate]();
+undefined -[HKWDeviceEventHandlerSingleton_setDelegate:]();
+undefined -[HKWDeviceEventHandlerSingleton_listenersTable]();
+undefined -[HKWDeviceEventHandlerSingleton_setListenersTable:]();
+undefined -[HKWDeviceEventHandlerSingleton_.cxx_destruct]();
+
