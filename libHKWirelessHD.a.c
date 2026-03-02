21109ad5e814ef2a0eac647eff9d53afcc3f0fea
hashtag8 <25273994+hashtag8@users.noreply.github.com>
2026-03-01 17:33:14 -0800

    Add files via upload

--- libHKWirelessHD.a.c
+++ libHKWirelessHD.a.c
@@ -1,3 +1,102 @@
-#include "libHKWirelessHD.a.h"
+typedef unsigned char   undefined;
+
+typedef unsigned int    dword;
+typedef struct linker_option_command linker_option_command, *Plinker_option_command;
+
+struct linker_option_command {
+    dword cmd;
+    dword cmdsize;
+    dword count;
+};
+
+typedef struct section section, *Psection;
+
+struct section {
+    char sectname[16];
+    char segname[16];
+    dword addr;
+    dword size;
+    dword offset;
+    dword align;
+    dword reloff;
+    dword nrelocs;
+    dword flags;
+    dword reserved1;
+    dword reserved2;
+};
+
+typedef struct dysymtab_command dysymtab_command, *Pdysymtab_command;
+
+struct dysymtab_command {
+    dword cmd;
+    dword cmdsize;
+    dword ilocalsym;
+    dword nlocalsym;
+    dword iextdefsym;
+    dword nextdefsym;
+    dword iundefsym;
+    dword nundefsym;
+    dword tocoff;
+    dword ntoc;
+    dword modtaboff;
+    dword nmodtab;
+    dword extrefsymoff;
+    dword nextrefsyms;
+    dword indirectsymoff;
+    dword nindirectsyms;
+    dword extreloff;
+    dword nextrel;
+    dword locreloff;
+    dword nlocrel;
+};
+
+typedef struct segment_command segment_command, *Psegment_command;
+
+struct segment_command {
+    dword cmd;
+    dword cmdsize;
+    char segname[16];
+    dword vmaddr;
+    dword vmsize;
+    dword fileoff;
+    dword filesize;
+    dword maxprot;
+    dword initprot;
+    dword nsects;
+    dword flags;
+};
+
+typedef struct symtab_command symtab_command, *Psymtab_command;
+
+struct symtab_command {
+    dword cmd;
+    dword cmdsize;
+    dword symoff;
+    dword nsyms;
+    dword stroff;
+    dword strsize;
+};
+
+typedef struct mach_header mach_header, *Pmach_header;
+
+struct mach_header {
+    dword magic;
+    dword cputype;
+    dword cpusubtype;
+    dword filetype;
+    dword ncmds;
+    dword sizeofcmds;
+    dword flags;
+};
+
+typedef struct linkedit_data_command linkedit_data_command, *Plinkedit_data_command;
+
+struct linkedit_data_command {
+    dword cmd;
+    dword cmdsize;
+    dword dataoff;
+    dword datasize;
+};
+
 
 
